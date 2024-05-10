dd7623c043b4caf49085272d1f136c758bea85d9
kms973 <newto9973@gmail.com>
2024-05-10 17:44:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { Route, Router, Routes, BrowserRouter } from 'react-router-dom';
+
+//component
+import Main from './component/Main';
+import Header from './component/header/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route exact path='/' Component={Main}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Main() {
+    return <>
+    <h2><Link to="">link1</Link></h2>
+    <h2><Link to="">link2</Link></h2>
+    </>
+}
